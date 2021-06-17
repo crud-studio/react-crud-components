ab2999b66dbfc55fd0f58a6bd5c5d6ff1e4c6ecd
Aviv Mor <mo.aviv@gmail.com>
2021-06-17 13:59:45 +0300

    Trying to resolve date issue.

--- src/entity/pages/details/inputs/EntityColumnInputDate.tsx
+++ src/entity/pages/details/inputs/EntityColumnInputDate.tsx
@@ -3,37 +3,41 @@ import {Controller, useFormContext} from "react-hook-form";
 import {IPropsEntityColumnInputType} from "../../../../models/props";
 import DatePickerWrapper from "../../../../components/inputs/DatePickerWrapper";
 import {useIntl} from "react-intl";
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
+import {LocalizationProvider} from "@material-ui/lab";
 
 const EntityColumnInputDate: FunctionComponent<IPropsEntityColumnInputType> = ({
-  column,
-  name,
-  disabled,
-  defaultValue,
-  onValueChanged,
-}) => {
-  const intl = useIntl();
-  const methods = useFormContext();
+                                                                                   column,
+                                                                                   name,
+                                                                                   disabled,
+                                                                                   defaultValue,
+                                                                                   onValueChanged,
+                                                                               }) => {
+    const intl = useIntl();
+    const methods = useFormContext();
 
-  return (
-    <Controller
-      name={name}
-      control={methods?.control}
-      rules={{required: column.required ? intl.formatMessage({id: "pages.required-field"}) : false}}
-      defaultValue={defaultValue || null}
-      render={({field}: any) => {
-        return (
-          <DatePickerWrapper
-            onChange={(value) => {
-              field?.onChange(value);
-              onValueChanged(value);
-            }}
-            value={field?.value}
-            disabled={disabled}
-            innerRef={field?.ref}
-          />
-        );
-      }}
-    />
-  );
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={intl.locale}>
+            <Controller
+                name={name}
+                control={methods?.control}
+                rules={{required: column.required ? intl.formatMessage({id: "pages.required-field"}) : false}}
+                defaultValue={defaultValue || null}
+                render={({field}: any) => {
+                    return (
+                        <DatePickerWrapper
+                            onChange={(value) => {
+                                field?.onChange(value);
+                                onValueChanged(value);
+                            }}
+                            value={field?.value}
+                            disabled={disabled}
+                            innerRef={field?.ref}
+                        />
+                    );
+                }}
+            />
+        </LocalizationProvider>
+    );
 };
 export default EntityColumnInputDate;
--- src/entity/pages/table/filters/TableFilterDate.tsx
+++ src/entity/pages/table/filters/TableFilterDate.tsx
@@ -2,105 +2,109 @@ import React, {FunctionComponent, useContext, useState} from "react";
 import {useEffectOnce, useUpdateEffect} from "react-use";
 import {FiltersContext} from "../../../managers/FilterManager";
 import _ from "lodash";
-import {DateRangePicker} from "@material-ui/lab";
+import {DateRangePicker, LocalizationProvider} from "@material-ui/lab";
 import {IconButton, InputAdornment, TextField} from "@material-ui/core";
 import {DateRange} from "@material-ui/lab/DateRangePicker/RangeTypes";
 import {Clear} from "@material-ui/icons";
 import {IPropsEntityColumnFilter} from "../../../../models/props";
 import {useIntl} from "react-intl";
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
 
 const TableFilterDate: FunctionComponent<IPropsEntityColumnFilter> = ({column}) => {
-  const intl = useIntl();
+    const intl = useIntl();
 
-  const {contextFilterFields, contextFilterFieldsClearedFlag, updateContextFilterField, removeContextFilterField} =
-    useContext(FiltersContext);
+    const {contextFilterFields, contextFilterFieldsClearedFlag, updateContextFilterField, removeContextFilterField} =
+        useContext(FiltersContext);
 
-  const [value, setValue] = useState<DateRange<Date>>([null, null]);
+    const [value, setValue] = useState<DateRange<Date>>([null, null]);
 
-  useEffectOnce(() => {
-    if (!!contextFilterFields?.length) {
-      const filterField = _.find(contextFilterFields, {fieldName: column.name});
-      if (filterField?.values && filterField?.values.length === 2) {
-        setValue([new Date(_.parseInt(filterField.values[0])), new Date(_.parseInt(filterField.values[1]))]);
-      }
-    }
-  });
+    useEffectOnce(() => {
+        if (!!contextFilterFields?.length) {
+            const filterField = _.find(contextFilterFields, {fieldName: column.name});
+            if (filterField?.values && filterField?.values.length === 2) {
+                setValue([new Date(_.parseInt(filterField.values[0])), new Date(_.parseInt(filterField.values[1]))]);
+            }
+        }
+    });
 
-  useUpdateEffect(() => {
-    if (!!value[0] || !!value[1]) {
-      setValue([null, null]);
-    }
-  }, [contextFilterFieldsClearedFlag]);
+    useUpdateEffect(() => {
+        if (!!value[0] || !!value[1]) {
+            setValue([null, null]);
+        }
+    }, [contextFilterFieldsClearedFlag]);
 
-  const onFilterChange = (filterValue: DateRange<Date>): void => {
-    setValue(filterValue);
-    if (!!filterValue[0] === !!filterValue[1]) {
-      onFilterValueChange(filterValue, true);
-    }
-  };
+    const onFilterChange = (filterValue: DateRange<Date>): void => {
+        setValue(filterValue);
+        if (!!filterValue[0] === !!filterValue[1]) {
+            onFilterValueChange(filterValue, true);
+        }
+    };
 
-  const onFilterValueChange = (filterValue: DateRange<Date>, debounced: boolean): void => {
-    if (!!filterValue[0] && !!filterValue[1]) {
-      updateContextFilterField(
-        {
-          fieldName: column.name,
-          operation: "Between",
-          values: [filterValue[0]?.getTime(), filterValue[1]?.getTime() + 86_400_000],
-        },
-        debounced
-      );
-    } else {
-      removeContextFilterField(column.name, debounced);
-    }
-  };
+    const onFilterValueChange = (filterValue: DateRange<Date>, debounced: boolean): void => {
+        if (!!filterValue[0] && !!filterValue[1]) {
+            updateContextFilterField(
+                {
+                    fieldName: column.name,
+                    operation: "Between",
+                    values: [filterValue[0]?.getTime(), filterValue[1]?.getTime() + 86_400_000],
+                },
+                debounced
+            );
+        } else {
+            removeContextFilterField(column.name, debounced);
+        }
+    };
 
-  const [closeFlag, setCloseFlag] = useState<number>(0);
+    const [closeFlag, setCloseFlag] = useState<number>(0);
 
-  const onClose = (): void => {
-    setCloseFlag((closeFlag) => closeFlag + 1);
-  };
+    const onClose = (): void => {
+        setCloseFlag((closeFlag) => closeFlag + 1);
+    };
 
-  useUpdateEffect(() => {
-    if (!!value[0] && !value[1]) {
-      setValue([null, null]);
-    }
-  }, [closeFlag]);
+    useUpdateEffect(() => {
+        if (!!value[0] && !value[1]) {
+            setValue([null, null]);
+        }
+    }, [closeFlag]);
 
-  return (
-    <DateRangePicker
-      onChange={onFilterChange}
-      onClose={onClose}
-      value={value}
-      disableMaskedInput
-      renderInput={({inputProps, ...startProps}, endProps) => {
-        const startValue = inputProps?.value;
-        const endValue = endProps.inputProps?.value;
-        delete inputProps?.value;
-        delete inputProps?.placeholder;
-        return (
-          <TextField
-            {...startProps}
-            label=""
-            helperText=""
-            placeholder={intl.formatMessage({id: "pages.search"})}
-            inputProps={inputProps}
-            value={!!startValue && !!endValue ? `${startValue} - ${endProps.inputProps?.value}` : ""}
-            size="small"
-            InputProps={{
-              readOnly: true,
-              endAdornment:
-                !!value[0] && !!value[1] ? (
-                  <InputAdornment position="end">
-                    <IconButton aria-label="clear filter" size="small" onClick={() => onFilterChange([null, null])}>
-                      <Clear fontSize="small" />
-                    </IconButton>
-                  </InputAdornment>
-                ) : undefined,
-            }}
-          />
-        );
-      }}
-    />
-  );
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={intl.locale}>
+            <DateRangePicker
+                onChange={onFilterChange}
+                onClose={onClose}
+                value={value}
+                disableMaskedInput
+                renderInput={({inputProps, ...startProps}, endProps) => {
+                    const startValue = inputProps?.value;
+                    const endValue = endProps.inputProps?.value;
+                    delete inputProps?.value;
+                    delete inputProps?.placeholder;
+                    return (
+                        <TextField
+                            {...startProps}
+                            label=""
+                            helperText=""
+                            placeholder={intl.formatMessage({id: "pages.search"})}
+                            inputProps={inputProps}
+                            value={!!startValue && !!endValue ? `${startValue} - ${endProps.inputProps?.value}` : ""}
+                            size="small"
+                            InputProps={{
+                                readOnly: true,
+                                endAdornment:
+                                    !!value[0] && !!value[1] ? (
+                                        <InputAdornment position="end">
+                                            <IconButton aria-label="clear filter" size="small"
+                                                        onClick={() => onFilterChange([null, null])}>
+                                                <Clear fontSize="small"/>
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ) : undefined,
+                            }}
+                        />
+                    );
+                }}
+            />
+        </LocalizationProvider>
+    );
 };
 export default TableFilterDate;
