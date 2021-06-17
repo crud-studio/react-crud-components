ad7c733a8d9c19742daffdbf815d0e069e646245
Aviv Mor <mo.aviv@gmail.com>
2021-06-17 14:36:32 +0300

    Reverted changes.

--- src/entity/pages/details/inputs/EntityColumnInputDate.tsx
+++ src/entity/pages/details/inputs/EntityColumnInputDate.tsx
@@ -3,8 +3,6 @@ import {Controller, useFormContext} from "react-hook-form";
 import {IPropsEntityColumnInputType} from "../../../../models/props";
 import DatePickerWrapper from "../../../../components/inputs/DatePickerWrapper";
 import {useIntl} from "react-intl";
-import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
-import {LocalizationProvider} from "@material-ui/lab";
 
 const EntityColumnInputDate: FunctionComponent<IPropsEntityColumnInputType> = ({
                                                                                    column,
@@ -17,27 +15,25 @@ const EntityColumnInputDate: FunctionComponent<IPropsEntityColumnInputType> = ({
     const methods = useFormContext();
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns} locale={intl.locale}>
-            <Controller
-                name={name}
-                control={methods?.control}
-                rules={{required: column.required ? intl.formatMessage({id: "pages.required-field"}) : false}}
-                defaultValue={defaultValue || null}
-                render={({field}: any) => {
-                    return (
-                        <DatePickerWrapper
-                            onChange={(value) => {
-                                field?.onChange(value);
-                                onValueChanged(value);
-                            }}
-                            value={field?.value}
-                            disabled={disabled}
-                            innerRef={field?.ref}
-                        />
-                    );
-                }}
-            />
-        </LocalizationProvider>
+        <Controller
+            name={name}
+            control={methods?.control}
+            rules={{required: column.required ? intl.formatMessage({id: "pages.required-field"}) : false}}
+            defaultValue={defaultValue || null}
+            render={({field}: any) => {
+                return (
+                    <DatePickerWrapper
+                        onChange={(value) => {
+                            field?.onChange(value);
+                            onValueChanged(value);
+                        }}
+                        value={field?.value}
+                        disabled={disabled}
+                        innerRef={field?.ref}
+                    />
+                );
+            }}
+        />
     );
 };
 export default EntityColumnInputDate;
