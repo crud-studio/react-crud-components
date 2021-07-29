c7bd16d50617e4f62e191e0e5500eb3cc46136a8
Aviv Mor <mo.aviv@gmail.com>
2021-07-29 17:37:10 +0300

    Fixed bugs in summary info.

--- src/components/cards/SummaryInfoCard.tsx
+++ src/components/cards/SummaryInfoCard.tsx
@@ -1,9 +1,10 @@
-import React, {FunctionComponent, ReactNode, useCallback, useRef} from "react";
+import React, {FunctionComponent, ReactNode, useCallback, useState} from "react";
 import {Box, Card, Divider, Stack, Tooltip, Typography} from "@material-ui/core";
 import {FormattedMessage} from "react-intl";
 import {SxProps} from "@material-ui/system";
 import {Theme} from "@material-ui/core/styles";
 import {CopyToClipboard} from "../../helpers/ClipboardUtils";
+import {useCallbackRef} from "../../hooks/useCallbackRef";
 
 interface IProps {
   info: {labelKey: string; value: ReactNode}[];
@@ -11,28 +12,11 @@ interface IProps {
 }
 
 const SummaryInfoCard: FunctionComponent<IProps> = ({info, sx}) => {
-  const valueRef = useRef<HTMLElement>();
-
-  const onValueClick = useCallback((value: ReactNode) => {
-    CopyToClipboard.text(valueRef.current?.innerText || "");
-  }, []);
-
   return (
     <Card sx={{overflowX: "auto", py: 3, ...sx}}>
       <Stack direction="row" divider={<Divider orientation="vertical" flexItem />}>
         {info.map((i, index) => (
-          <Stack width={1} sx={{textAlign: "center", minWidth: 150, px: 2}} key={index}>
-            <Typography variant="caption" noWrap sx={{mb: 0.5, color: "text.secondary", display: "block"}}>
-              <FormattedMessage id={i.labelKey} />
-            </Typography>
-            <Typography variant="subtitle1" noWrap>
-              <Tooltip title={<>{i.value}</>}>
-                <Box component="span" ref={valueRef} onClick={() => onValueClick(i.value)} sx={{cursor: "pointer"}}>
-                  {i.value}
-                </Box>
-              </Tooltip>
-            </Typography>
-          </Stack>
+          <SummaryInfoValue labelKey={i.labelKey} value={i.value} key={index} />
         ))}
       </Stack>
     </Card>
@@ -40,3 +24,28 @@ const SummaryInfoCard: FunctionComponent<IProps> = ({info, sx}) => {
 };
 
 export default SummaryInfoCard;
+
+const SummaryInfoValue: FunctionComponent<{labelKey: string; value: ReactNode}> = ({labelKey, value}) => {
+  const [valueText, setValueText] = useState<string>("");
+
+  const valueRef = useCallbackRef<HTMLElement>(null, (current) => setValueText(current?.innerText || ""));
+
+  const onValueClick = useCallback(() => {
+    CopyToClipboard.text(valueText);
+  }, [valueText]);
+
+  return (
+    <Stack width={1} sx={{textAlign: "center", minWidth: 150, px: 2}}>
+      <Typography variant="caption" noWrap sx={{mb: 0.5, color: "text.secondary", display: "block"}}>
+        <FormattedMessage id={labelKey} />
+      </Typography>
+      <Typography variant="subtitle1" noWrap>
+        <Tooltip title={valueText}>
+          <Box component="span" ref={valueRef} onClick={onValueClick} sx={{cursor: "pointer"}}>
+            {value}
+          </Box>
+        </Tooltip>
+      </Typography>
+    </Stack>
+  );
+};
